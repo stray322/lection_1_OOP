16c3ae5a4db6a2fa09aa18074374a72b7854d7d8
annasolyannikova57 <60076564+stray322@users.noreply.github.com>
2024-10-14 10:27:38 +0400

    Delete src/lection_2/WrongPasswordException.java

--- src/lection_2/WrongPasswordException.java
+++
@@ -1,9 +0,0 @@
-package lection_2;
-
-public class WrongPasswordException extends Exception{
-    public WrongPasswordException(){
-    }
-    public WrongPasswordException(String message){
-        super(message);
-    }
-}
