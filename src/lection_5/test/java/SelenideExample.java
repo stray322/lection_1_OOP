b195650cd765420aa31a496000df941d5e7129c8
stray322 <60076564+stray322@users.noreply.github.com>
2024-11-26 20:43:51 +0300

    Выполнено задание 5.

---
+++ src/lection_5/test/java/SelenideExample.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
+
+public class SelenideExample {
+    @Test
+    public void test1() {
+        open("https://zhcom.ru/");
+        $(By.cssSelector("a[class='_js-link-flats']"))
+                .shouldBe(visible)
+                .shouldHave(text("Квартиры"))
+                .click();
+        $(By.cssSelector("a[href='/#projects']"))
+                .shouldBe(visible)
+                .click();
+        webdriver().shouldHave(url("https://zhcom.ru/#projects"));
+        $(By.xpath("//*[contains(@name, 'SubscriptionForm[name]')]"))
+                .setValue("Анна")
+                .click();
+        $(By.xpath("//*[contains(@name, 'SubscriptionForm[email]')]"))
+                .setValue("tsybizova.anya@mail.ru")
+                .click();
+        sleep(5000);
+    }
+}
